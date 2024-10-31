c7fdcab1b88e3a56a10abac04e833a0bf70840e2
Demetrio Junior <dm3o@live.com>
2024-10-31 14:57:43 -0300

    31/10 14h57

---
+++ UserServices/Program.cs
@@ -0,0 +1,26 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.Run("locahost:5001/users");
+
+
+app.MapGet("/{*id}", (string? id, HttpContext context) =>
+{
+
+});
+
+app.MapPost("/", (HttpContext context) =>
+{
+
+});
+
+app.MapPut("/{*id}", (string id, HttpContext context) =>
+{
+
+});
+
+app.MapDelete("/{*id}", (string id) =>
+{
+
+});
