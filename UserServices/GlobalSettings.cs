21bb2048529c505aca851c0e52dbe9acbb2b88ee
Demetrio M. Junior <71155627+demetriomjr@users.noreply.github.com>
2024-11-16 13:36:14 -0300

    16/11 12h30

---
+++ UserServices/GlobalSettings.cs
@@ -0,0 +1,10 @@
+﻿global using System.IdentityModel.Tokens.Jwt;
+global using System.Diagnostics;
+global using System.Security.Claims;
+global using System.Text;
+
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.EntityFrameworkCore;
+
+global using Models;
+global using Databases;
