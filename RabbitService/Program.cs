6515ce57ed256d4bc92bb4c454b3ce165459d393
Demetrio M. Junior <71155627+demetriomjr@users.noreply.github.com>
2024-11-18 08:23:58 -0300

    Delete RabbitService directory

--- RabbitService/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿var factory = new ConnectionFactory() { HostName = "localhost"};
-using var connection = await factory.CreateConnectionAsync();
-using var channel = await connection.CreateChannelAsync();
-
-var UserRequestQueue = await DeclareQueue("user_requests");
-var UserResponseQueue = await DeclareQueue("user_responses");
-
